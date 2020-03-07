b23c299de77ca0b122cc2479db0a8e28a93352df
Thomas Wiffler <thomas@thomas.lan1>
2020-03-06 20:45:48 -0600

    end of chapter 12

--- ship.py
+++ ship.py
@@ -1,9 +1,10 @@
 import pygame
-
+ 
 class Ship:
-    """A class to manage the ship"""
+    """A class to manage the ship."""
+ 
     def __init__(self, ai_game):
-        "Initialize the ship and set its starting position"
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
@@ -12,27 +13,27 @@ class Ship:
         self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
 
-        # Start each new ship at the bottom center of the screen
+        # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Store a decimal value for the ship's horizontal position
+        # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
-        # Movement flag
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on the movement flag."""
-        # Update the ship's x value, not the rect
-        if self.moving_right:
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
-            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
 
-            # Update rect object from self.x
-            self.rect.x = self.x
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
-        """Draw the ship at its current location"""
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
